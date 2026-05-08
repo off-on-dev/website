19113b7b5b21817dc4ff4a7bdae7ee8b3a36a488
Sinduri Guntupalli <sinduri.guntupalli@dynatrace.com>
2026-05-08 16:52:29 +0200

    fix: ignore generated router types in lint
    Signed-off-by: Sinduri Guntupalli <sinduri.guntupalli@dynatrace.com>

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "coverage", "src/components/ui/**"] },
+  { ignores: ["dist", "coverage", "src/components/ui/**", ".react-router/types/**"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
