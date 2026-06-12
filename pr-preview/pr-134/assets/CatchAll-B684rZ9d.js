f1860723b61b912095f18aa2f0eb390030abf36b
sinduri-g <sinduri-g@users.noreply.github.com>
2026-06-12 09:26:09 +0000

    Deploy preview for PR 134 🛫

--- pr-preview/pr-134/assets/CatchAll-B684rZ9d.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./NotFound-1ghkWeOS.js";export{t as default,e as meta};
--- pr-preview/pr-134/assets/contributors-DFkWiNhk.js
+++
@@ -1,1 +0,0 @@
-var e={name:`Katharina Sick`,url:`https://ksick.dev/`,about:`DevRel at Dynatrace and co-organizer of Cloud Native Linz. Passionate about building user-friendly Cloud Native and Kubernetes solutions, with a background in mobile and backend development. Found in tech and sports communities, inline skating rinks, and quiz nights across Europe.`};export{e as t};
